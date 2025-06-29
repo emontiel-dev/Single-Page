331637a93cc9f87ac249ed1e5e9ab944fa87c942
Essau-dev <emontiel.pers@gmail.com>
2025-06-29 01:06:46 -0600

    implemento configuracion para despliegue en github pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  // Configura la URL base para GitHub Pages
+  // Reemplaza '/Single-Page/' con el nombre exacto de tu repositorio si es diferente
+  base: '/Single-Page/',
+
   // Opciones de configuración de Vite (si las necesitas)
   // Por ejemplo, si tu index.html no está en la raíz:
   // root: './src',
